f4acae30e1dc548aed9617ec683a1c1499c9a109
UtecMartinTito <martin.tito@utec.edu.pe>
2021-09-15 22:53:57 -0500

    Se agregan ajustes en el Main

--- src/main/java/cs/ing/software/lab1/Main.java
+++ src/main/java/cs/ing/software/lab1/Main.java
@@ -1,15 +1,19 @@
 package cs.ing.software.lab1;
+import java.util.logging.Logger;
 
 public class Main {
-
+    static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[ ] args) {
 //Ejemplo automovil
         Automovil lAutomovil1 = new Automovil(10,100,0.9);
-        lAutomovil1.recorrido(4);
-        lAutomovil1.recorrido(12);
+        String caso1 = lAutomovil1.recorrido(4);
+        logger.info(caso1);
+        String caso2 = lAutomovil1.recorrido(12);
+        logger.info(caso2);
 
 //Ejemplo camion
         Camion lCamion = new Camion(10,100,1.6);
-        lCamion.recorrido(4);
+        String caso3 = lCamion.recorrido(4);
+        logger.info(caso3);
     }
 }
